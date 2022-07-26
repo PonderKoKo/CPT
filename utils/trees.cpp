446c78b4d5b9e21bb6958a7f136190e390e6a794
KoKonuts <ponderkoko@gmail.com>
2022-07-26 18:31:03 +0200

    Fix UpdateSegmentTree, and Sum preset

--- utils/trees.cpp
+++ utils/trees.cpp
@@ -57,27 +57,29 @@ public:
     }
 };
 
-template<typename T, T neutral, T f(T,T)>
+/* You can have range queries and updates, so long as you can calculate an update for a range
+ * u_func(previous, length, lazy) -> new value
+ * l_func(old_lazy, additional_lazy) -> new lazy value
+ * The functions will be allowed to be passed as lambdas as of C++20 :(
+ */
+template<typename T, T q_neutral, T q_func(T,T), typename T_lazy, T_lazy u_neutral, T_lazy u_func(T, u32, T_lazy), T_lazy l_func(T_lazy, T_lazy)>
 class UpdateSegmentTree {
 private:
     vector<T> a;
-    vector<T> lazy; /* for + only */
+    vector<T_lazy> lazy;
     u32 n;
 public:
-    explicit UpdateSegmentTree(const vector<T>& init) : a(4 * init.size(), neutral /* necessary? */), n(init.size()), lazy(4 * init.size(), neutral) {
+    explicit UpdateSegmentTree(const vector<T>& init) : a(4 * init.size(), q_neutral), lazy(4 * init.size(), u_neutral), n(init.size()) {
         build(init, 1, 0, n);
     }
 
-    explicit UpdateSegmentTree(u32 size) : a(4 * size, neutral /* necessary? */), lazy(4 * size, neutral), n(size) {
-        vector<T> init(size, neutral);
-        build(init, 1, 0, n);
-    }
+    explicit UpdateSegmentTree(u32 size) : a(4 * size, q_neutral), lazy(4 * size, u_neutral), n(size) { }
 
     const T& build(const vector<T>& init, u32 node, u32 l, u32 r) {
         if (l + 1 == r)
             return a[node] = init[l];
         u32 mid = (l + r) / 2;
-        return a[node] = f(build(init, node * 2, l, mid),
+        return a[node] = q_func(build(init, node * 2, l, mid),
                            build(init, node * 2 + 1, mid, r));
     }
 
@@ -92,12 +94,12 @@ public:
 
     T query(u32 node, u32 l, u32 r, u32 ql, u32 qr) {
         if (ql >= r || qr <= l) // [l, r) and [ql, qr) are disjoint
-            return neutral;
+            return q_neutral;
         lazily(node, l, r);
         if (ql <= l && r <= qr) // [l, r) is a subset of [ql, qr)
             return a[node];
         u32 mid = (l + r) / 2;
-        return f(query(node * 2, l, mid, ql, qr), query(node * 2 + 1, mid, r, ql, qr));
+        return q_func(query(node * 2, l, mid, ql, qr), query(node * 2 + 1, mid, r, ql, qr));
     }
 
     void set(u32 i, T val) {
@@ -112,7 +114,7 @@ public:
         if (l + 1 == r)
             return a[node] = val;
         u32 mid = (l + r) / 2;
-        return a[node] = f(set(node * 2, l, mid, i, val), set(node * 2 + 1, mid, r, i, val));
+        return a[node] = q_func(set(node * 2, l, mid, i, val), set(node * 2 + 1, mid, r, i, val));
     }
 
     void update (u32 l, u32 r, T val) {
@@ -125,23 +127,30 @@ public:
         if (ql >= r || qr <= l) // [l, r) and [ql, qr) are disjoint
             return a[node];
         if (ql <= l && r <= qr) { // [l, r) is a subset of [ql, qr)
-            lazy[node] += val;
+            lazy[node] = val;
             return lazily(node, l, r);
         }
         u32 mid = (l + r) / 2;
-        return a[node] = f(update(node * 2, l, mid, ql, qr, val),
+        return a[node] = q_func(update(node * 2, l, mid, ql, qr, val),
                            update(node * 2 + 1, mid, r, ql, qr, val));
     }
 
     const T& lazily (u32 node, u32 l, u32 r) {
-        if (lazy[node] == neutral)
+        if (lazy[node] == u_neutral)
             return a[node];
-        a[node] += static_cast<i32>(r - l) * lazy[node];
+        a[node] = u_func(a[node], r - l, lazy[node]);
         if (l + 1 < r) {
-            lazy[node * 2] += lazy[node];
-            lazy[node * 2 + 1] += lazy[node];
+            lazy[node * 2] = l_func(lazy[node * 2], lazy[node]);
+            lazy[node * 2 + 1] = l_func(lazy[node * 2 + 1], lazy[node]);
         }
-        lazy[node] = neutral;
+        lazy[node] = u_neutral;
         return a[node];
     }
 };
+// C++17:
+i64 sum(i64 a, i64 b) { return a + b; }
+i64 sum_lazy(i64 a, u32 len, i64 b) { return a + static_cast<i64>(len) * b; }
+using sumST = UpdateSegmentTree<i64, 0, sum, i64, 0, sum_lazy, sum>;
+
+// C++20:
+// using sumST = UpdateSegmentTree<i64, 0, [](i64 a, i64 b) { return a + b; }, i64, 0, [](i64 a, u32 len, i64 b) { return a + static_cast<i64>(len) * b; }, [](i64 a, i64 b) { return a + b; }>;
