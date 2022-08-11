188520c7a09d09fe69d70c7495b78986782f8888
KoKonuts <ponderkoko@gmail.com>
2022-08-11 17:42:50 +0200

    Add Dinic

--- utils/traversal.cpp
+++ utils/traversal.cpp
@@ -50,18 +50,89 @@ void treeDFS(u32 node, const Graph& children) {
 }
 
 
-vector<u32> topo_sort (const Graph& adj, u32 n) {
-    vector<u32> in_degree(n, 0);
+vector<u32> topo_sort (const Graph& adj) {
+    vector<u32> in_degree(adj.size(), 0);
     for (const auto &v : adj)
         for (const auto u : v)
             in_degree[u]++;
     vector<u32> res;
-    rep(i,n)
+    rep(i,adj.size())
         if (!in_degree[i])
             res.push_back(i);
-    rep(i,n)
+    rep(i,adj.size())
         for (u32 v : adj[res[i]])
             if (!--in_degree[v])
                 res.push_back(v);
     return res;
 }
+
+/* Dinic's algorithm for Max Flow
+ * Complexity: O(|V|^2 * |E|)
+ */
+template <typename flow_type>
+class dinic {
+    struct edge {
+        size_t src, dst, rev;
+        flow_type flow, cap;
+    };
+
+    int n;
+    vector<vector<edge>> adj;
+
+    explicit dinic(int n) : n(n), adj(n), level(n), q(n), it(n) {}
+
+    void add_edge(size_t src, size_t dst, flow_type cap, flow_type rcap = 0) {
+        adj[src].push_back({src, dst, adj[dst].size(), 0, cap});
+        if (src == dst)
+            adj[src].back().rev++;
+        adj[dst].push_back({dst, src, adj[src].size() - 1, 0, rcap});
+    }
+
+    vector<int> level, q, it;
+
+    bool bfs(int source, int sink) {
+        fill(level.begin(), level.end(), -1);
+        for (int qf = level[q[0] = sink] = 0, qb = 1; qf < qb; ++qf) {
+            sink = q[qf];
+            for (edge &e : adj[sink]) {
+                edge &r = adj[e.dst][e.rev];
+                if (r.flow < r.cap && level[e.dst] == -1)
+                    level[q[qb++] = e.dst] = 1 + level[sink];
+            }
+        }
+        return level[source] != -1;
+    }
+
+    flow_type augment(int source, int sink, flow_type flow) {
+        if (source == sink)
+            return flow;
+        for (; it[source] != adj[source].size(); ++it[source]) {
+            edge &e = adj[source][it[source]];
+            if (e.flow < e.cap && level[e.dst] + 1 == level[source]) {
+                flow_type delta = augment(e.dst, sink, min(flow, e.cap - e.flow));
+                if (delta > 0) {
+                    e.flow += delta;
+                    adj[e.dst][e.rev].flow -= delta;
+                    return delta;
+                }
+            }
+        }
+        return 0;
+    }
+
+    flow_type max_flow(int source, int sink) {
+        for (vector<edge>& u : adj)
+            for (edge& e : u)
+                e.flow = 0;
+        flow_type flow = 0;
+        flow_type oo = numeric_limits<flow_type>::max();
+
+        while (bfs(source, sink)) {
+            fill(it.begin(), it.end(), 0);
+            for (flow_type f; (f = augment(source, sink, oo)) > 0;)
+                flow += f;
+
+        } // level[u] = -1 => source side of min cut
+        return flow;
+    }
+};
