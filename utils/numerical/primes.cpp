ff2468d95e8a64c9b36d845a93c7276bff8eee00
KoKonuts <89521080+PonderKoKo@users.noreply.github.com>
2024-01-31 12:42:17 +0100

    Change to linear-time sieve that cleanly returns prime list and smallest factor.

--- utils/numerical/primes.cpp
+++ utils/numerical/primes.cpp
@@ -1,17 +1,16 @@
 #include "../macros.h"
 
-auto sieve(num n) {
-    vector<bool> is_prime(n, true);
-    is_prime[0] = is_prime[1] = false;
-    for (num i = 2; i < n; ++i)
-        if (is_prime[i])
-            for (num j = i * i; j < n; j += i)
-                is_prime[j] = false;
-    return is_prime;
-}
-
-seq primes(num n) {
-    auto is_prime = sieve(n);
-    auto view = views::iota(2ll, n) | views::filter([&] (num x) { return is_prime[x]; });
-    return {all(view)};
+// Linear-time sieve returning list of primes and smallest factor of each number.
+auto sieve(int N) {
+    vector<int> p, q(N);
+    for (int i = 2; i < N; i++) {
+        if (!q[i])
+            p.push_back(q[i] = i);
+        for (int j : p) {
+            if (j > q[i] || i * j >= N)
+                break;
+            q[i * j] = j;
+        }
+    }
+    return pair{p, q};
 }
