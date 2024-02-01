77f6f92b975d9f0777a679e3657b9e19694b935b
KoKonuts <89521080+PonderKoKo@users.noreply.github.com>
2024-02-01 15:44:50 +0100

    Improve Matrix template

--- utils/types/matrix.cpp
+++ utils/types/matrix.cpp
@@ -2,46 +2,37 @@
 
 template<typename T>
 struct Matrix {
-    num rows, cols;
     Table<T> a;
-    Matrix(const Table<T>& t) : rows{ssize(t)}, cols{ssize(t.front())}, a{t} {}
-    Matrix(num r, num c) : rows{r}, cols{c}, a(r, vector<T>(c)) {}
+
+    explicit Matrix(const Table<T>& t) : a{t} {}
+    explicit Matrix(const vector<T>& v) : a{v} {}
+    Matrix(int r, int c) : a(r, vector<T>(c)) {}
+    explicit Matrix(int rc, bool id = false) : Matrix(rc, rc) {
+        if (id) rep(i, rc) a[i][i] = 1;
+    }
+
+    int rows() const { return size(a); }
+    int cols() const { return size(a[0]); }
 
     Matrix operator* (const Matrix& other) const {
-        assert(cols == other.rows);
-        Matrix result(rows, other.cols);
-        rep(i, rows) rep(j, other.cols) rep(k, cols)
-        result.a[i][j] += a[i][k] * other.a[k][j];
+        assert(cols() == other.rows());
+        Matrix result(rows(), other.cols());
+        rep(i, rows()) rep(k, cols()) rep(j, other.cols())
+                    result.a[i][j] += a[i][k] * other.a[k][j];
         return result;
     }
 
-    vector<T> operator* (const vector<T>& vec) const {
-        assert(cols == ssize(vec));
-        vector<T> result(rows);
-        rep(i, rows) rep(j, cols)
-        result[i] += a[i][j] * vec[j];
-        return result;
+    Matrix expoMultiply(num power, Matrix v) const {
+        assert(rows() == cols() && v.cols() == rows());
+        assert(power >= 0);
+        for (Matrix m{*this}; power; power >>= 1, m *= m)
+            if (power & 1)
+                v *= m;
+        return v;
     }
 
     Matrix operator^ (num power) const {
-        assert(rows == cols);
-        assert(power >= 0);
-        Matrix result(rows, cols);
-        rep(i, rows)
-            result.a[i][i] = 1;
-        for (Matrix m{*this}; power; power >>= 1, m *= m)
-            if (power & 1)
-                result *= m;
-        return result;
-    }
-
-    vector<T> expoMultiply(num power, vector<T> vec) const {
-        assert(rows == cols);
-        assert(power >= 0);
-        for (Matrix m{*this}; power; power >>= 1, m *= m)
-            if (power & 1)
-                vec = m * vec;
-        return vec;
+        return expoMultiply(power, Matrix(rows(), true));
     }
 
     Matrix& operator*= (const Matrix& other) { return *this = *this * other; }
