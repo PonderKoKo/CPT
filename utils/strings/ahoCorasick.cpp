1a1ced497719fd946c53913dffbbc2606a13f357
KoKonuts <89521080+PonderKoKo@users.noreply.github.com>
2024-02-03 13:02:36 +0100

    Trim some code that can be written / adapted on the fly

--- utils/strings/ahoCorasick.cpp
+++ utils/strings/ahoCorasick.cpp
@@ -5,64 +5,44 @@ struct AhoCorasick {
     enum {sigma = 26, alpha = 'a'};
     struct Node {
         array<int,sigma> next{0};
-        int prev = 0, count = 0, end = -1;
-        // All optional but prev depends on end. Just delete all lines that mention the removed var.
+        int count = 0, longest = -1;
+        // vector<int> matches;
+        // All optional, delete unneeded lines.
         // count -> number of matches ending at c
         // end -> index of longest pattern ending at c
-        // prev -> index of next node with match (for enumerating all matches)
+        // matches -> indices of all patterns ending at c. At most (Sum P)^3/2 if no duplicates.
     };
     vector<Node> t;
 
-    int& f(int v, char c, bool sub = 1) { return t[v].next[c - alpha * sub]; }
+    int& f(int v, char c) { return t[v].next[c - alpha]; }
 
-    explicit AhoCorasick(const vector<string>& p) : t(transform_reduce(all(p), 1, plus<>(), [] (const string& x) { return size(x); })) {
-        for (int i = 1, j = 0; const string& s : p) {
+    explicit AhoCorasick(const vector<string>& p) : t(1) {
+        rep(i, ssize(p)) {
             int v = 0;
-            for (auto c : s)
-                v = f(v, c) ?: f(v, c) = i++;
+            for (char c : p[i])
+                v = f(v, c) ?: f(v, c) = (t.emplace_back(), size(t) - 1);
             t[v].count++;
-            t[v].end = j++;
+            t[v].longest = i;
+            // t[v].matches.push_back(i);
         }
         for (queue<pair<int,int>> q{{{0, 0}}}; !q.empty(); q.pop()) {
             auto [v, link] = q.front();
-            rep(c, sigma)
-                if (!f(v, c, 0))
-                    f(v, c, 0) = f(link, c, 0);
+            for (char c = alpha; c < alpha + sigma; c++)
+                if (f(v, c))
+                    q.emplace(f(v, c), v ? f(link, c) : 0);
                 else
-                    q.emplace(f(v, c, 0), v ? f(link, c, 0) : 0);
+                    f(v, c) = f(link, c);
             t[v].count += t[link].count;
-            t[v].prev = t[link].end == -1 ? t[link].prev : link;
+            if (t[v].longest == -1) t[v].longest = t[link].longest;
+            // t[v].matches.insert(end(t[v].matches), all(t[link].matches));
         }
     }
 
     // Returns states traversed in automaton
-    vector<int> match(const string& s, int v) {
+    vector<int> match(const string& s, int v = 0) {
         vector<int> ans;
         for (char c : s)
             ans.push_back(v = f(v, c));
         return ans;
-    }
-
-    // Pass in state-vector obtained from match.
-    num count(const vector<int>& a) {
-        return transform_reduce(all(a), 0ll, plus<>(), [&] (int v) { return t[v].count; });
-    }
-
-    vector<int> longest(const vector<int>& a) {
-        vector<int> b(size(a));
-        transform(all(a), begin(b), [&] (int v) { return t[v].end; });
-        return b;
-    }
-
-    Table<int> allMatches(const vector<int>& a) {
-        Table<int> b;
-        for (int v : a) {
-            b.emplace_back();
-            do {
-                if (t[v].end != -1)
-                    b.back().push_back(t[v].end);
-            } while (v = t[v].prev);
-        }
-        return b;
     }
 };
