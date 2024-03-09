bd13ff637f32e9c72594c9913da63ca6be1d6e8f
KoKonuts <89521080+PonderKoKo@users.noreply.github.com>
2024-03-09 15:09:43 +0100

    Add Binary Search

--- utils/data structures/SegTreeL.cpp
+++ utils/data structures/SegTreeL.cpp
@@ -12,7 +12,7 @@ class SegTreeL {
     op(_query, qn, q[v],)
     op(_update, q[v], (u[v] = qx, push(v, l, r), q[v]), q[v] =)
     op(_set, q[v], q[v] = qy, q[v] =)
-
+#undef op
     void push(int v, int l, int r) {
         q[v] = uf(q[v], u[v], l, r);
         if (l + 1 < r)
@@ -24,47 +24,20 @@ class SegTreeL {
     Q build(const vector<Q> &a, int v, int l, int r) {
         return q[v] = l + 1 == r ? a[l] : qf(build(a, v + 1, l, (l + r) / 2), build(a, v + (r - l & ~1), (l + r) / 2, r));
     }
+
+    int search(int v, int l, int r, auto& p) {
+        push(v, l, r);
+        return ql >= r ? 0 : ql <= l && p(qf(qy, q[v]), r) ? qy = qf(qy, q[v]), 0 : l + 1 == r ? r :
+        search(v + 1, l, (l + r) / 2, p) ?: search(v + (r - l & ~1), (l + r) / 2, r, p);
+    }
+
 public:
     SegTreeL(int n) : n(n), q(2 * n - 1, qn), u(2 * n - 1, un) {}
     SegTreeL(const vector<Q> &a) : SegTreeL(size(a)) { build(a, 0, 0, n); }
     Q query(int l, int r) { return ql = l, qr = r, _query(0, 0, n); }
     void update(int l, int r, U x) { ql = l, qr = r, qx = x, _update(0, 0, n); }
     void set(int i, Q y) { ql = i, qr = i + 1, qy = y, _set(0, 0, n); }
+    // Returns minimum r > l, s. t. p(query(l, r), r) is false or 0 if always true.
+    int search(int l, auto p) { return ql = l, qy = qn, search(0, 0, n, p); }
 };
-using SumAddST = SegTreeL<num,num,0,0,plus<>{},[](num x, num y, int l, int r) { return x + y * (r - l); },plus<>{}>;
-
-
-/* Assuming p: query([ql, x)) × interval_length -> bool is monotonic in x.
-   * Returns the index of the first element x, such that query([ql, x)) is true.
-   * If no such element exists, returns n + 1
-   * Complexity is O(log n) rather than O((log n)^2) by just binary searching
-   * from the outside
-   */
-/*
-num search(num ql, const function<bool(T, num)> &p) {
-    T acc_val = q_neutral;
-    num acc_len = 0;
-    return search(1, 0, n, ql, n, acc_val, acc_len, p);
-}
-
-num search(num node, num l, num r, num ql, num qr, T &acc_val, num &acc_len,
-           const function<bool(T, num)> &p) {
-    if (r <= ql)
-        return n + 1; // sentinel value
-    if (l >= qr)
-        return l; // Why isn't this r?
-    lazily(node, l, r);
-    if (l >= ql && r <= qr && !p(q_func(acc_val, a[node]), acc_len + r - l)) {
-        acc_val = q_func(acc_val, a[node]);
-        acc_len += r - l;
-        return n + 1;
-    }
-    if (l + 1 == r)
-        return l; // Why isn't this r?
-
-    num mid = (l + r) / 2;
-    num res = search(node * 2, l, mid, ql, qr, acc_val, acc_len, p);
-    if (res != n + 1)
-        return res;
-    return search(node * 2 + 1, mid, r, ql, qr, acc_val, acc_len, p);
-}*/
+using MaxAddST = SegTreeL<num,num,numeric_limits<num>::min(),0,ranges::max,[](num x, num y, int l, int r) { return x + y; },plus<>{}>;
