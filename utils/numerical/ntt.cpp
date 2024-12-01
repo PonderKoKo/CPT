d1fab0249b00d48bc073fdd0dd3997224315e156
KoKonuts <89521080+PonderKoKo@users.noreply.github.com>
2024-12-01 15:50:10 +0100

    Update mint and ntt

--- utils/numerical/ntt.cpp
+++ utils/numerical/ntt.cpp
@@ -1,43 +1,40 @@
 #include "../macros.h"
 #include "../types/mint.cpp"
 
-template<num m, num r>
-void ntt(vector<mint<m>>& a) {
-    num n = ssize(a), L = bit_width(size(a)) - 1;
-    static vector<mint<m>> rt(2, 1);
+vector<mint> convolve(vector<mint> a, vector<mint> b) {
+    assert(m == 998244353);
+    static vector<mint> rt(2, 1);
+
+    ull s = size(a) + size(b) - 1, n = 2 * bit_floor(s);
+    mint inverse = !mint(n);
     for (static int k = 2; k < n; k *= 2) {
         rt.resize(n);
-        auto z{mint<m>{r} ^ m / 2 / k};
+        mint z = 3; // !
+        z ^= m / 2 / k;
         rep(i, k)
             rt[i + k] = rt[(i + k) / 2] * (~i & 1 ?: z);
     }
+
     vector<int> rev(n);
-    rep(i,n)
-        rev[i] = (rev[i / 2] | (i & 1) << L) / 2;
-    rep(i,n)
-        if (i < rev[i])
-            swap(a[i], a[rev[i]]);
-    for (int k = 1; k < n; k *= 2) {
-        for (int i = 0; i < n; i += 2 * k) {
-            rep(j, k) {
-                auto z = rt[j + k] * a[i + j + k];
-                a[i + j + k] = a[i + j] - z;
-                a[i + j] += z;
+    rep(i, n) rev[i] = (rev[i / 2] | (i & 1) * n) / 2;
+    auto ntt = [&] (vector<mint>& a) {
+        rep(i, n) if (i < rev[i]) swap(a[i], a[rev[i]]);
+        for (int k = 1; k < n; k *= 2) {
+            for (int i = 0; i < n; i += 2 * k) {
+                rep(j, k) {
+                    mint z = rt[j + k] * a[i + j + k];
+                    a[i + j + k] = a[i + j] - z;
+                    a[i + j] += z;
+                }
             }
         }
-    }
-}
-
-template<num r = 3, num m = 998244353>
-vector<mint<m>> convolve(vector<mint<m>> a, vector<mint<m>> b) {
-    ull s = size(a) + size(b) - 1, n = 2 * bit_floor(s);
-    auto inverse = !mint<m>(n);
-    vector<mint<m>> out(n);
+    };
+    vector<mint> out(n);
     a.resize(n), b.resize(n);
-    ntt<r,m>(a), ntt<r,m>(b);
+    ntt(a), ntt(b);
     rep(i, n)
-        out[-i & (n - 1)] = a[i] * b[i] * inverse;
-    ntt<r,m>(out);
+        out[-i & n - 1] = a[i] * b[i] * inverse;
+    ntt(out);
     return {begin(out), begin(out) + s};
 }
-// https://judge.yosupo.jp/submission/244769
+// https://judge.yosupo.jp/submission/253806
--- utils/types/mint.cpp
+++ utils/types/mint.cpp
@@ -1,34 +1,32 @@
 #pragma once
 #include "../macros.h"
 
-template <num m = 4611686018427377339, int s = 0>
+constexpr num m = ~42256ull/4;
 struct mint {
-	num a;
-	constexpr void ain() { if (a < 0) a += m; }
-	constexpr mint(num x = 0) : a{x % m} { ain(); }
-	#define op(n, T, a, b) \
-	mint& operator n ## = (T o) { a; return *this b; }  \
-	friend mint operator n (mint x, T y) { return x n ## = y; }
-	op(+, mint, a += o.a - m; ain(),)
-	op(-, mint, a -= o.a; ain(),)
-	op(*, mint, a = (conditional_t<m<1<<30,num,__int128>) a * o.a % m,)
-	op(/, mint, , *= !o)
-	op(^, mint<m-1>, for (mint x{exchange(*this, 1)}; o.a; o.a /= 2, x *= x) if (o.a & 1) *this *= x,)
-	op(<<, num, , *= b[o]; )
-	op(>>, num, , *= b[s+1] * b[s-o]; )
-	#undef op
-	mint operator!() const { return *this ^ -1; }
-	mint operator-() const { return -a; }
-	friend istream& operator>>(istream& is, mint& x) { return is >> x.a, x.a %= m, x.ain(), is; }
-	friend ostream& operator<<(ostream& os, mint x) { return os << x.a; }
-	auto operator<=>(const mint& other) const = default;
-	static mint *b;
+    num a;
+    mint(num x = 0) : a{x % m} { *this -= 0; }
+#define op(n, T, a, b) \
+    mint& operator n ## = (T o) { a; return *this b; } \
+    friend mint operator n (mint x, T y) { return x n ## = y; }
+    op(+, mint, a += o.a; if (a >= m) a -= m,)
+    op(-, mint, a -= o.a; if (a < 0) a += m,)
+    op(*, mint, a = __int128(a) * o.a % m,)
+    op(/, mint, , *= !o)
+    op(^, num, o %= m - 1; if (o < 0) o += m - 1; mint x = *this; for (*this = 1; o; o /= 2, x *= x) if (o & 1) *this *= x,)
+    auto operator<=>(const mint& other) const = default;
+    
+    mint operator!() const { return *this ^ -1; }
+    mint operator-() const { return -a; }
+    friend istream& operator>>(istream& is, mint& x) { num z; return is >> z, x = z, is; }
+    friend ostream& operator<<(ostream& os, mint x) { return os << x.a; }
+    
+    static mint b(int i) {
+        static vector<mint> a{1, 1e9 + rng()}, c{1, 1 / a[1]};
+        auto& z = i < 0 ? i = abs(i), c : a;
+        while (size(z) <= i) z.push_back(z[1] * z.back());
+        return z[i];
+    }
+    op(<<, num, , *= b(o))
+    op(>>, num, , *= b(-o))
+#undef op
 };
-template<num m, int s> mint<m, s> *mint<m, s>::b = [] {
-	b = new mint<m,s>[s + 2];
-	b[0] = 1;
-	b[1] = uniform_int_distribution<num>(1e8, 2e8)(rng);
-	rep(i, s - 1) b[i+2] = b[i + 1] * b[1];
-	b[s + 1] = !b[s];
-	return b;
-}();
