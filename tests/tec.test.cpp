01c325dbc2790693aea6ce41e727f00767a54309
KoKonuts <89521080+PonderKoKo@users.noreply.github.com>
2025-01-23 20:07:38 +0100

    Add test for TEC

---
+++ tests/tec.test.cpp
@@ -0,0 +1,21 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/two_edge_connected_components"
+
+#include "../utils/graph/tec.cpp"
+
+auto solve() {
+    int n, m;
+    cin >> n >> m;
+    Table<int> adj(n);
+    rep(i, m) {
+        int u, v;
+        cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+    auto c = tec(adj);
+    Table<int> comps(ranges::max(c) + 1);
+    rep(i, n) comps[c[i]].push_back(i);
+    cout << size(comps) << '\n';
+    for (const auto& x : comps)
+        cout << size(x) << ' ' << x << '\n';
+}
--- utils/graph/tec.cpp
+++ utils/graph/tec.cpp
@@ -5,19 +5,18 @@
 // Bridges are the edges connecting two components.
 // adj must be symmetric. Multi-edges and self-loops are handled.
 vector<int> tec(const Table<int>& adj) {
-    int n = size(adj), t = 0;
-	vector<int> a(n), b(n), z(n + 1);
+    int n = size(adj), t = 0, c = 0;
+	vector<int> a(n), b(n), z(n);
     function<int(int,int)> f = [&] (int u, int x) {
         if (b[u]) return (0 < x) - (x < 0);
-        z[b[u] = ++t] = u;
-        x = !x - 1;
+        z[t] = u; b[u] = ++t; x = !x - 1;
         for (int v : adj[u])
             x += f(v, b[u] - b[v]);
-        while (!x && ++z[0] && b[u] <= t)
-            a[z[t--]] = --z[0];
+        if (!x && --c) while (b[u] <= t)
+            a[z[--t]] = ~c;
         return x;
     };
     rep(u, n) f(u, 0);
     return a;
 }
-// https://judge.yosupo.jp/submission/252571
+// https://judge.yosupo.jp/submission/262950
