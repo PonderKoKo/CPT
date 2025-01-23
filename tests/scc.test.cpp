80d5a3d4c96fe23f2fbe77eb2ae7b3dc98b8aca2
KoKonuts <89521080+PonderKoKo@users.noreply.github.com>
2025-01-23 20:04:47 +0100

    Add test for SCC

---
+++ tests/scc.test.cpp
@@ -0,0 +1,20 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/scc"
+
+#include "../utils/graph/scc.cpp"
+
+auto solve() {
+    int n, m;
+    cin >> n >> m;
+    Table<int> adj(n);
+    rep(i, m) {
+        int u, v;
+        cin >> u >> v;
+        adj[u].push_back(v);
+    }
+    auto c = scc(adj);
+    Table<int> comps(ranges::max(c) + 1);
+    rep(i, n) comps[c[i]].push_back(i);
+    cout << size(comps) << '\n';
+    for (const auto& x : comps | views::reverse)
+        cout << size(x) << ' ' << x << '\n';
+}
--- utils/graph/scc.cpp
+++ utils/graph/scc.cpp
@@ -1,25 +1,23 @@
 #include "../macros.h"
 
-/* Strongly Connected Components in Directed Graph in O(V + E)
- * Returns vector with mapping to components (1-indexed, maximum is number of components).
+/* Strongly Connected Components in Directed Graph in O(n + m)
+ * Returns vector with mapping to components (0-indexed).
  * Component Indices are in Reverse Topological Order (only edges to smaller components)
  */
 vector<int> scc(const Table<int>& adj) {
-    vector<int> a(size(adj)), b{a}, z;
-    int t{0}, c{0};
-    function<int(int)> f = [&] (int u) {
-        int d = b[u] = ++t;
-        z.push_back(u);
+    int n = size(adj), t = 0, c = 0;
+    vector<int> a(n), b(n), z(n);
+    function<void(int)> f = [&] (int u) {
+        if (b[u]) return;
+        z[t] = u;
+        b[u] = ++t;
         for (int v : adj[u])
-            if (!a[v])
-                d = min(d, b[v] ?: f(v));
-        if (d == b[u] && ++c)
-            while (!a[u])
-                a[z.back()] = c, z.pop_back();
-        return b[u] = d;
+            if (f(v), b[v] < b[u])
+                b[u] = b[v], a[u] = 1;
+        if (a[u] && --c) while (b[u] <= t)
+            a[z[--t]] = ~c, b[z[t]] = 1e9;
     };
-    rep(u, size(adj))
-    	a[u] || f(u);
+    rep(u, n) f(u);
     return a;
 }
-// https://judge.yosupo.jp/submission/244338
+// https://judge.yosupo.jp/submission/263122
