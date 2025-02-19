1197bd179e5e92f63f429a3c2d5a99b5e1bdbb26
KoKonuts <89521080+PonderKoKo@users.noreply.github.com>
2025-02-19 15:03:32 +0100

    Add Lagrange interpolation for sums of powers

---
+++ utils/numerical/interpolate.cpp
@@ -0,0 +1,19 @@
+#include "../types/mint.cpp"
+
+// Assuming a = [P(0), P(1), ..., P(n-1)], computes P(x) for the unique polynomial P of degree n - 1.
+// Complexity: O(n + log mod)
+mint interpolate(vector<mint> a, num x) {
+    mint z = 1; rep(i, size(a)) z *= i + 1; z = !z;
+    rep(i, size(a)) a[i] *= z *= size(a) - i, end(a)[~i] *= z * (-i % 2 ?: 1);
+    for (mint& q : a) q *= z, z *= x--;
+    for (z = 1; mint& q : a | views::reverse) q *= z, z *= ++x;
+    return reduce(all(a));
+}
+
+// Computes Sum_{i=0}^{n-1} i^p [with (0^0 = 1)]
+// Complexity: O(p log p)
+mint power_sum(num p, num n) {
+    vector<mint> a(p + 2);
+    rep(i, p + 1) a[i + 1] = a[i] + (mint(i) ^ p);
+    return interpolate(a, n);
+}
