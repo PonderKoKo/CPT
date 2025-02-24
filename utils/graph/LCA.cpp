19975f948953a7f3807dab48631b98a1258aa3b2
KoKonuts <89521080+PonderKoKo@users.noreply.github.com>
2025-02-24 13:57:21 +0100

    Progress

--- utils/graph/LCA.cpp
+++ utils/graph/LCA.cpp
@@ -1,40 +1,37 @@
-#include "../macros.h"
 #include "../data structures/SparseTable.cpp"
 
 struct LCA {
-    int t = 0;
-    vector<int> d, o, r, q;
+    vector<int> d, o, r, q = d;
     SparseTable<int,ranges::min> st;
-    LCA(const Table<int>& adj, int root = 0) : d(size(adj)), q(d), st((dfs(root, -1, adj), r)) {}
-
-    void dfs(int u, int p, const Table<int>& adj) {
-        q[u] = t++;
+    LCA(const Table<int>& adj, int root = 0) : d(size(adj)), st((dfs(root, adj), r)) {}
+    void dfs(int u, const Table<int>& adj) {
+        q[u] = size(o);
+        d[u]++;
         for (int v : adj[u])
-            if (v != p)
-                d[v] = d[u] + 1, o.push_back(u), r.push_back(q[u]), dfs(v, u, adj);
+            if (!d[v])
+                d[v] = d[u], o.push_back(u), r.push_back(q[u]), dfs(v, adj);
     }
 
-    int query(int u, int v) const {
-        if (u == v || min(u, v) < 0)
-            return max(u, v);
-        if (q[u] > q[v])
-            swap(u, v);
-        return o[st.query(q[u], q[v])];
+    int query(int u, int v) {
+        if ((u ^ v) <= 0)
+            return u & v;
+        tie(u, v) = minmax(q[u], q[v]);
+        return o[st.query(u, v)];
     }
 
-    int dist(int u, int v) const {
+    int dist(int u, int v) {
         return d[u] + d[v] - 2 * d[query(u, v)];
     }
 
     // Computes virtual tree in O(k log k).
-    // Children are stored in aux (overwrites previous calls). Returns root of virtual tree.
-    int virtualTree(vector<int> a, Table<int>& adj) {
-        ranges::sort(a, {}, [&] (int x) { return q[x]; });
-        for (int i = size(a); --i;) a.push_back(query(a[i - 1], a[i]));
-        ranges::sort(a, {}, [&] (int x) { return q[x]; });
-        a.erase(unique(all(a)), end(a));
-        stack<int> s{{a[0]}};
-        for (int x : a) {
+    // Children are stored in adj (overwrites previous calls). Returns root of virtual tree.
+    int virtual_tree(vector<int> a, Table<int>& adj) {
+        rep(k, 2) {
+            rep(i, k * size(a) / 2)
+                a.push_back(query(a[i], a[i + 1]));
+            ranges::sort(a, {}, [&] (int x) { return q[x]; });
+        }
+        for (stack<int> s{{a[0]}}; int x : a) {
             while (query(s.top(), x) != s.top())
                 s.pop();
             adj[s.top()].push_back(x);
