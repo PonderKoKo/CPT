a9a507df0350015f29489e196b72993cc9fb4dd3
KoKonuts <89521080+PonderKoKo@users.noreply.github.com>
2025-05-26 23:35:38 +0200

    Improve code quality in lattice attack

--- utils/numerical/lattice_attack.py
+++ utils/numerical/lattice_attack.py
@@ -1,15 +1,13 @@
 import flint
 
 
-def collide(moduli, bases, sigma, c=100, n=1):
-    assert sigma >= 8, "Lattice Reduction Attack doesn't work well for small alphabets"
-    assert len(moduli) == len(bases)
+def collide(mbs, sigma, c, n=1):
     m = flint.fmpz_mat([
-        [0] * i + [1] + [0] * (n - i - 1) + [c * pow(b, i, m) for m, b in zip(moduli, bases)]
+        [j == i for j in range(n)] + [c * pow(b, i, m) for m, b in mbs]
         for i in range(n)
     ]).lll().tolist()
-    return next(filter(lambda x: not any(x[-len(moduli):]) and max(map(abs, x)) < sigma, m), None) \
-        or collide(moduli, bases, sigma, c, n + 1)
+    return next(filter(lambda x: not any(x[-len(mbs):]) and max(map(abs, x)) < sigma, m), None) \
+        or collide(mbs, sigma, c, n + 1)
 
 
 def collide_strings(alpha, collision):
@@ -22,13 +20,11 @@ def h(s, modulus, base):
 
 def main():
     alpha, sigma = 'a', 26
-    moduli = [993019889, 997084241]
-    bases = [307] * 2
-    col = collide(moduli, bases, sigma, 8)
+    mbs = [(993019889, 307), (997084241, 307)]
+    col = collide(mbs, sigma, 8)
     s, t = collide_strings(alpha, col[:-1])
     print(s, t)
-    for m, b in zip(moduli, bases):
-        assert h(s, m, b) == h(t, m, b), "String Hashes should match"
+    assert all(h(s, m, b) == h(t, m, b) for m, b in mbs), "String Hashes should match"
 
 
 if __name__ == "__main__":
