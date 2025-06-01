16cad63c01727805ff8b52ab36b53a272deb23fb
KoKonuts <89521080+PonderKoKo@users.noreply.github.com>
2025-06-01 22:06:20 +0200

    Fix bug

--- utils/types/mint.cpp
+++ utils/types/mint.cpp
@@ -4,12 +4,12 @@
 constexpr num mod = 998244353; // ~42256ull/4; // for hashing
 struct mint {
     conditional_t<mod<1<<31,int,num> a;
-    mint(num x = 0) : a(x % mod) { if (a < mod) a += mod; }
+    mint(num x = 0) : a(x % mod) { if (a < 0) a += mod; }
 #define op(n, T, a, b) \
 mint& operator n ## = (T o) { a; return *this b; } \
 friend mint operator n (mint x, T y) { return x n ## = y; }
     op(+, mint, a += o.a; if (a >= mod) a -= mod,)
-    op(-, mint, a -= o.a; if (a < mod) a += mod,)
+    op(-, mint, a -= o.a; if (a < 0) a += mod,)
     op(*, mint, a = (conditional_t<mod<1<<31,num,__int128>) a * o.a % mod,)
     op(/, mint, , *= !o)
     op(^, num, o %= mod - 1; if (o < 0) o += mod - 1; mint x = *this; for (*this = 1; o; o /= 2, x *= x) if (o & 1) *this *= x,)
